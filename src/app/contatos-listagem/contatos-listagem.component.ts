f85e71250e32759930065f6da3880f993395df95
Moisés Alcântara <msalcmd@gmail.com>
2020-12-23 02:01:53 -0300

    primeiro commit, pega REST

---
+++ src/app/contatos-listagem/contatos-listagem.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+
+@Component({
+  selector: 'app-contatos-listagem',
+  templateUrl: './contatos-listagem.component.html',
+  styleUrls: ['./contatos-listagem.component.css']
+})
+export class ContatosListagemComponent implements OnInit {
+
+  contatos : Array<any>;
+
+  constructor(private contatoService: ContatoService) { }
+
+  ngOnInit(): void {
+    this.listar();
+  }
+
+  listar(){
+    this.contatoService.listar().subscribe(dados => this.contatos = dados);
+  }
+
+}
